8ae7fb3d69f5acd5011ededd3a93cd2fc85fc974
XenoRx <franboise123@gmail.com>
2024-02-14 15:46:06 +0300

    added: String Filter class

---
+++ gbtest/StringFilter.cs
@@ -0,0 +1,21 @@
+﻿namespace gbtest
+{
+    internal class StringFilter
+    {
+        public static string[] FilterStrings(string[] array)
+        {
+            string[] result = new string[array.Length];
+            int count = 0;
+            foreach (string str in array)
+            {
+                if (str.Length <= 3)
+                {
+                    result[count] = str;
+                    count++;
+                }
+            }
+            Array.Resize(ref result, count);
+            return result;
+        }
+    }
+}
